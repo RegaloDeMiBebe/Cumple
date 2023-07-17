c84e2df3152f06da5daee5ff4d16cfe29f0597f3
RegaloDeMiBebe <139659538+RegaloDeMiBebe@users.noreply.github.com>
2023-07-16 21:54:55 -0600

    Vela anadida

---
+++ src/script3.js
@@ -0,0 +1,20 @@
+$(function () {
+  var flame = $('#flame');
+  var txt = $('h1');
+
+  flame.on(
+    {
+      click: function () {
+        flame.removeClass('burn').addClass('puff');
+        $('.smoke').each(function () {
+          $(this).addClass('puff-bubble');
+        });
+        $('#glow').remove();
+        txt.html("Felices <b>18</b> Amor de mi vida, Guadalupe♥️...").delay(10550).fadeOut(800);
+        $('#candle').animate(
+          {
+            //'opacity': '.5'
+          }, 100);
+      }
+    })
+});
